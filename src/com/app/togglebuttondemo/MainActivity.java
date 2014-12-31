b479718199987a55889fcf6a289796490cb63fe9
sym <932390312@qq.com>
2014-12-31 13:58:20 +0800

    sym init

---
+++ src/com/app/togglebuttondemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.app.togglebuttondemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity implements OnCheckedChangeListener {
+	private ToggleButton tButton;
+	private  ImageView  image;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		//初始化变量
+		tButton=(ToggleButton) findViewById(R.id.ToggleButton);
+		image=(ImageView) findViewById(R.id.ImageView);
+		//设置监听器
+		tButton.setOnCheckedChangeListener(this);
+	}
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		image.setBackgroundResource(isChecked?R.drawable.nan:R.drawable.meinv);
+	}
+	
+
+}
